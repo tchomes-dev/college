25edb43bc08492a2d483d867c503c897be958f39
tchomes-dev <tonychoma322@gmail.com>
2020-10-06 00:39:50 -0400

    HW05

--- Computing 3 Assignments/HW05/hw5.cpp
+++ Computing 3 Assignments/HW05/hw5.cpp
@@ -15,20 +15,22 @@ public:
     bool operator>(const Rational& b);
     bool operator>=(const Rational& b);
     // arithmtic operator overload declarations
-    Rational* operator+(const Rational b);
-    Rational* operator-(const Rational b);
-    Rational* operator*(const Rational b);
-    Rational* operator/(const Rational b);
+    Rational* operator+(Rational& b);
+    Rational* operator-(Rational& b);
+    Rational* operator*(Rational& b);
+    Rational* operator/(Rational& b);
     // input/output operator overload declarations
     ostream& operator<<(ostream& out);
     istream& operator>>(istream& in);
     //other
 
-
-
 private:
-    int numerator;
-    int denominator;
+    int numerator = 0;
+    int denominator = 0;
+
+    int greatestCommDenom(const Rational& a);
+    int greatestCommDenom(int a, int b);
+    void simplify(Rational& a);
 };
 
 // constructor definition
@@ -43,8 +45,14 @@ Rational::Rational(int initWholeNumber) {
 }
 
 Rational::Rational(int initNumerator, int initDenominator) {
-    this->numerator = initNumerator;
-    this->denominator = initDenominator;
+    if (initDenominator < 0) {
+        initNumerator *= -1;
+        initDenominator *= -1;
+    } else {
+        this->numerator = initNumerator;
+        this->denominator = initDenominator;
+    }
+
 }
 
 // relational operator overload definition
@@ -98,19 +106,30 @@ bool Rational::operator>=(const Rational& b) {
 }
 
 // arithmetic operator overload definition
-Rational* Rational::operator+(const Rational b) { 
+Rational* Rational::operator+(Rational& b) { 
+    this->numerator = (this->numerator * b.denominator) + (this->denominator * b.numerator);
+    this->denominator *= b.denominator;
     return this;
 }
 
-Rational* Rational::operator-(const Rational b) { 
+Rational* Rational::operator-(Rational& b) { 
+    this->numerator = (this->numerator * b.denominator) - (this->denominator * b.numerator);
+    this->denominator *= b.denominator;
+    simplify(*this);
     return this;
 }
 
-Rational* Rational::operator*(const Rational b) { 
+Rational* Rational::operator*(Rational& b) { 
+    this->numerator *= b.numerator;
+    this->denominator *= b.denominator;
+    simplify(*this);
     return this;
 }
 
-Rational* Rational::operator/(const Rational b) { 
+Rational* Rational::operator/(Rational& b) { 
+    this->numerator *= b.denominator;
+    this->denominator *= b.numerator;
+    simplify(*this);
     return this;
 }
 
@@ -124,6 +143,47 @@ istream& Rational::operator>>(istream& in) {
     return in; 
 }
 
+//other 
+int Rational::greatestCommDenom(const Rational& fraction) {
+    int gcd = greatestCommDenom(fraction.numerator, fraction.denominator);
+    int a = fraction.numerator;
+    int b = fraction.denominator;
+
+    while (b != 0) {
+        int t = b;
+        b = a % b;
+        a = t;
+    }
+    return gcd;
+}
+int Rational::greatestCommDenom(int a, int b) {
+    while (b != 0) {
+        int t = b;
+        b = a % b;
+        a = t;
+    }
+    return a;
+}
+//reduces a rational number to its simplist form
+void Rational::simplify(Rational& a) {
+    int gcd = greatestCommDenom(a);
+    a.numerator /= gcd;
+    a.denominator /= gcd;
+}
+
 int main(int argv, char argc[]) { 
+    Rational a(1, 2);
+    Rational b(1, 6);
     return 0; 
 }
+
+/*
+Computing III -- COMP.2010 Honor Statement
+The practice of good ethical behavior is essential for maintaining good order in the classroom, providing an enriching learning experience for students, and as training as a practicing computing professional upon graduation. This practice is manifested in the University’s Academic Integrity policy. Students are expected to strictly avoid academic dishonesty and adhere to the Academic Integrity policy as outlined in the course catalog. Violations will be dealt with as outlined therein. All programming assignments in this class are to be done by the student alone. No outside help is permitted except the instructor and approved tutors.
+
+I certify that the work submitted with this assignment is mine and was generated in a manner consistent with this document, the course academic policy on the course website on Blackboard, and the UMass
+Lowell academic code.
+
+Date: 10-6-2020
+Name: Tony Choma
+*/
