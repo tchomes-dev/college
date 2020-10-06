1105f186354125a5ad5e21ebaf003b0e12bd1a17
tchomes-dev <tonychoma322@gmail.com>
2020-10-06 02:16:10 -0400

    HW05 update

--- Computing 3 Assignments/HW05/hw5.cpp
+++ Computing 3 Assignments/HW05/hw5.cpp
@@ -1,3 +1,11 @@
+/***********************************************
+Author: <Tony Choma>
+Date: <10-6-2020>
+Purpose: <Overload operators to support a custom class for rational numbers>
+Sources of Help: <stackoverflow, wikipedia>
+Time Spent: <5 hours>
+***********************************************/
+
 #include <iostream>
 
 using namespace std;
@@ -179,11 +187,21 @@ int main(int argv, char argc[]) {
 
 /*
 Computing III -- COMP.2010 Honor Statement
-The practice of good ethical behavior is essential for maintaining good order in the classroom, providing an enriching learning experience for students, and as training as a practicing computing professional upon graduation. This practice is manifested in the University’s Academic Integrity policy. Students are expected to strictly avoid academic dishonesty and adhere to the Academic Integrity policy as outlined in the course catalog. Violations will be dealt with as outlined therein. All programming assignments in this class are to be done by the student alone. No outside help is permitted except the instructor and approved tutors.
-
-I certify that the work submitted with this assignment is mine and was generated in a manner consistent with this document, the course academic policy on the course website on Blackboard, and the UMass
+The practice of good ethical behavior is essential for maintaining
+good order in the classroom, providing an enriching learning
+experience for students, and as training as a practicing computing
+professional upon graduation. This practice is manifested in the
+University’s Academic Integrity policy. Students are expected to
+strictly avoid academic dishonesty and adhere to the Academic
+Integrity policy as outlined in the course catalog. Violations will
+be dealt with as outlined therein.
+All programming assignments in this class are to be done by the
+student alone. No outside help is permitted except the instructor and
+approved tutors.
+I certify that the work submitted with this assignment is mine and was
+generated in a manner consistent with this document, the course
+academic policy on the course website on Blackboard, and the UMass
 Lowell academic code.
-
 Date: 10-6-2020
 Name: Tony Choma
 */
