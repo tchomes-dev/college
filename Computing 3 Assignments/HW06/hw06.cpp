165b87d49c253ceab2b8cf3971cbc4db1c167dc2
tchomes-dev <tonychoma322@gmail.com>
2020-10-16 00:39:26 -0400

    hw6

--- Computing 3 Assignments/HW06/hw06.cpp
+++ Computing 3 Assignments/HW06/hw06.cpp
@@ -1,5 +1,23 @@
 #include <string>
+#include <vector>
+
+using namespace std;
 
 class DynamicStringArray {
+public:
+    //constructor
+    DynamicStringArray() {
+        this->dynamicArray[nullptr];
+        this->size = 0;
+    }
+    //member functions
+    int getSize() { return this->size; }
+    void addEntry(string entry);
+    void deleteEntry(string entry);
+    string getEntry(int index);
+    
 
+private:
+    vector<string> dynamicArray;
+    int size;
 };
