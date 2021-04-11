2e5d6e3a21a817170cd02ced5c8c1867791a157e
tchomes-dev <tonychoma322@gmail.com>
2021-04-11 08:21:39 -0400

    bhidashbkads

--- College/Computing 4 Assignments/ps5/TFractal.cpp
+++ College/Computing 4 Assignments/ps5/TFractal.cpp
@@ -1,9 +1,12 @@
 #include "Triangle.h"
 
-void fTree(std::vector<Triangle> shapes, int N, double L, sf::Vector2f center);
+void fTree(std::vector<Triangle>* shapes, int N, double L, sf::Vector2f center);
 
 int main(int argc, char* argv[]) {
-    sf::RenderWindow window(sf::VideoMode(800, 800), "Planets");
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+
+    sf::RenderWindow window(sf::VideoMode(800, 800), "Sierpinski Triangles", sf::Style::Default, settings);
 
     window.setFramerateLimit(60);
 
@@ -17,22 +20,22 @@ int main(int argc, char* argv[]) {
         }
         window.clear();
         sf::Vector2f center(window.getSize().x / 2, window.getSize().y / 2);
-        std::vector<Triangle> shapes;
-        int N = 4;
-        int L = 400;
+        std::vector<Triangle>* shapes = new vector<Triangle>;
+        int N = stod(argv[2]);
+        int L = stod(argv[1]);
         fTree(shapes, N, L, center);
-        for(auto s : shapes) window.draw(s);
+        for(auto s : *shapes) window.draw(s);
         window.display();
     }
 }
 
-void fTree(std::vector<Triangle> shapes, int N, double L, sf::Vector2f center) {
+void fTree(std::vector<Triangle>* shapes, int N, double L, sf::Vector2f center) {
     int H = L * cosf(M_PI / 6);
-    if (N <= 0) return;
+    if (N < 0) return;
     else {
-        shapes.push_back(Triangle(L, center));
-        fTree(shapes, N-1, L/2, sf::Vector2f(center.x - L / 2, center.y - (2 / 3) * H));
-        fTree(shapes, N-1, L/2, sf::Vector2f(center.x + (3 / 4) * L, center.y - (1 / 6) * H));
-        fTree(shapes, N - 1, L / 2, sf::Vector2f(center.x - (1 / 4) * L, center.y + (5 / 6) * H));
+        shapes->push_back(Triangle(L, center));
+        fTree(shapes, N-1, L/2, sf::Vector2f(center.x - L / 2, center.y - (2 * H) / 3));
+        fTree(shapes, N-1, L/2, sf::Vector2f(center.x + (3 * L) / 4, center.y - (1 * H) / 6));
+        fTree(shapes, N - 1, L / 2, sf::Vector2f(center.x - (1 * L) / 4, center.y + (5 * H) / 6));
     }
 }
