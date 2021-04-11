9cf170198a823f5e96355e2fec42405d202edf86
Tony <tonychoma322@gmail.com>
2021-04-11 07:15:32 -0400

    ndsla

--- College/Computing 4 Assignments/ps5/TFractal.cpp
+++ College/Computing 4 Assignments/ps5/TFractal.cpp
@@ -1,6 +1,6 @@
 #include "Triangle.h"
 
-void fTree(sf::RenderWindow& w, int N, double L, sf::Vector2f center);
+void fTree(std::vector<Triangle> shapes, int N, double L, sf::Vector2f center);
 
 int main(int argc, char* argv[]) {
     sf::RenderWindow window(sf::VideoMode(800, 800), "Planets");
@@ -17,25 +17,22 @@ int main(int argc, char* argv[]) {
         }
         window.clear();
         sf::Vector2f center(window.getSize().x / 2, window.getSize().y / 2);
+        std::vector<Triangle> shapes;
         int N = 4;
         int L = 400;
-        Triangle t(L, center);
-        //window.draw(t);
-        fTree(window, N, L, center);
+        fTree(shapes, N, L, center);
+        for(auto s : shapes) window.draw(s);
         window.display();
     }
 }
 
-void fTree(sf::RenderWindow& w, int N, double L, sf::Vector2f center) {
-    Triangle t(L, center);
+void fTree(std::vector<Triangle> shapes, int N, double L, sf::Vector2f center) {
     int H = L * cosf(M_PI / 6);
-    if (N <= 0) {
-        w.draw(t);
-        return;
-    } else {
-        fTree(w, N-1, L/2, sf::Vector2f(center.x - L / 2, center.y - (2 / 3) * H));
-        fTree(w, N-1, L/2, sf::Vector2f(center.x + (3 / 4) * L, center.y - (1 / 6) * H));
-        fTree(w, N - 1, L / 2, sf::Vector2f(center.x - (1 / 4) * L, center.y + (5 / 6) * H));
-        w.draw(t);
+    if (N <= 0) return;
+    else {
+        shapes.push_back(Triangle(L, center));
+        fTree(shapes, N-1, L/2, sf::Vector2f(center.x - L / 2, center.y - (2 / 3) * H));
+        fTree(shapes, N-1, L/2, sf::Vector2f(center.x + (3 / 4) * L, center.y - (1 / 6) * H));
+        fTree(shapes, N - 1, L / 2, sf::Vector2f(center.x - (1 / 4) * L, center.y + (5 / 6) * H));
     }
 }
